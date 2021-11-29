60b7ced37b12dc3169e680846bbcc299258c58e7
Arthur Gomes <arthurgds4@gmail.com>
2021-11-29 11:18:52 -0300

    adiconando função apagar tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,24 @@ const add = document.querySelector('#add');
 const task = document.querySelector('#task');
 
 function creatList(){
-    const list = document.createElement('h6');
+    const list = document.createElement('li');
+    list.setAttribute('class', 'list');
     return list;
+}
+function createButton(list){
+    const deleteTask = document.createElement('button');
+    deleteTask.innerText = 'Delete';
+    deleteTask.setAttribute('class', 'delete');
+    list.appendChild(deleteTask);
 }
 
 function creatTask(textInput){
     const list = creatList();
     list.innerHTML=textInput;
     task.appendChild(list);
-    
+    createButton(list);
 }
+
 inputTask.addEventListener('keypress', function(e){
     if(e.keyCode === 13){
         if(!inputTask.value) return;
@@ -26,3 +34,9 @@ add.addEventListener('click', function(){
     inputTask.value='';
 })
 
+document.addEventListener('click', function(event){
+    const elementClicked = event.target;
+    if(elementClicked.classList.contains('delete')){
+        elementClicked.parentElement.remove();
+    }
+})
